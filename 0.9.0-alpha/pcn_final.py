Fixed the computation of the best number of clusters k, when user choose k_choice='best_k'

0.9.0-alpha/pcn_final.py
@@ -501,9 +501,6 @@ def hardSpectralClustering(A, n_clusters = None, norm=False, embedding=None, d=2
             L = compute_laplacian_matrix(A)
 
         sortedEigenvalues, sortedEigenvectors = computeSortEigens(L)             #sorted eigenvalues/eigenvectors
-            
-        if n_clusters is None:
-            n_clusters = computeBestK(sortedEigenvalues, n_k = 1)
         
         train = sortedEigenvectors[:, :n_clusters]
         if norm:
@@ -537,9 +534,6 @@ def softSpectralClustering(A, n_clusters = None, norm=False, embedding = None,  
             L = compute_normalized_laplacian(A)
         else:
             L = compute_laplacian_matrix(A)
-        
-        if n_clusters is None:
-            n_clusters = computeBestK(sortedEigenvalues,  n_k = 1)
             
         sortedEigenvalues, sortedEigenvectors = computeSortEigens(L)            #sorted eigenvalues/eigenvectors
         train = sortedEigenvectors[:, :n_clusters]
@@ -548,7 +542,7 @@ def softSpectralClustering(A, n_clusters = None, norm=False, embedding = None,  
         if norm:
             train = sortedEigenvectors/np.sqrt(np.sum(sortedEigenvectors**2))
             train = train[:, :n_clusters]
-            
+        
     else:
         if (embedding in supported_embeddings):
              
@@ -579,9 +573,6 @@ def ssc_shimalik(A, n_clusters = None, embedding=None, d=2, beta=0.01):
         idx = eigenvalues.argsort()
         sortedEigenvalues = eigenvalues[idx].real
         sortedEigenvectors = eigenvectors[:,idx].real
-
-        if n_clusters is None:
-            n_clusters = computeBestK(sortedEigenvalues)
 
         train = sortedEigenvectors[:, :n_clusters]
        

0.9.0-alpha/pcn_main.py
@@ -261,16 +261,16 @@ while (end==False):
                                
                         n_of_best_ks = int(input("Enter the number of best_ks to try: "))
                           
-                        if('ShiMalik' in algorithm_choice):
+                        if('shimalik' in algorithm_choice):
                             L = pcn_final.compute_laplacian_matrix(A)
                             D = pcn_final.degree_matrix(A) 
                             eigenvalues = eigh(L, D, eigvals_only=True)   
                                 
-                        elif('Norm' in algorithm_choice):
+                        elif('norm' in algorithm_choice):
                             L = pcn_final.compute_normalized_laplacian(A)
                             eigenvalues, eigenvectors  = np.linalg.eig(L)    
                                
-                        else: #'Unnorm' in algorithm_choice
+                        else: #'unnorm' in algorithm_choice
                             L = pcn_final.compute_laplacian_matrix(A)
                             eigenvalues, eigenvectors = np.linalg.eig(L)
                                 