Fixed an error in the Adj matrix computation. Added the possibility to extract a single chain from the pdb file: to use in case of protein complexes and protein polymers (proteins formed by the union of identical proteins).

0.9.0-alpha/pcn_final.py
@@ -89,23 +89,40 @@ def checkIfFilesExists(files, initial_choice, proteins_path, adj_path = None):
                 output_path = os.path.abspath(os.path.join(adj_path, os.pardir))+add_slash_to_path
                 A = adjacent_matrix(output_path, residues, p_name, min_, max_)
             
-            all_adj_file_exists = True
-                   
+            all_adj_file_exists = True                   
     
-def readPDBFile(pbdFilePath):
+def readPDBFile(pdbFilePath, chain = None):
+
     atoms = []
-    with open(pbdFilePath) as pdbfile:
+    with open(pdbFilePath) as pdbfile:
         for line in pdbfile:
             if line[:4] == 'ATOM':
               # Split the line
                 splitted_line = [line[:6], line[6:11], line[12:16], line[17:20], line[21], line[22:26], line[30:38], line[38:46], line[46:54], line[56:61], line[62:66]]
-                atoms.append(splitted_line)
-                
+                if chain is None:
+                    atoms.append(splitted_line)
+                else:
+                    if(line[21] == chain.upper()):
+                        atoms.append(splitted_line)
                 # To format again the pdb file with the fields extracted
                 #print("%-6s%5s %4s %3s %s %4s    %8s%8s%8s   %3s%3s\n"%tuple(splitted_line))
-    return np.array(atoms)
+    return np.array(atoms)    
 
-def getResidueDistance(atoms):
+def getAllChainsFromPDB(pdbFilePath):
+
+    chains = []
+    with open(pdbFilePath) as pdbfile:
+        for line in pdbfile:
+            if line[:4] == 'ATOM':
+              # Split the line
+                splitted_line = [line[:6], line[6:11], line[12:16], line[17:20], line[21], line[22:26], line[30:38], line[38:46], line[46:54], line[56:61], line[62:66]]
+                if(line[21] not in chains):
+                    chains.append(line[21])
+                # To format again the pdb file with the fields extracted
+                #print("%-6s%5s %4s %3s %s %4s    %8s%8s%8s   %3s%3s\n"%tuple(splitted_line))
+    return np.array(chains)
+    
+def getResidueDistance(atoms, chain = None):
   
     residues = []
     residues_list = ['ALA', 'CYS', 'ASP', 'GLU', 'PHE', 'GLY', 'HIS', 'ILE', 'LYS', 
@@ -115,10 +132,9 @@ def getResidueDistance(atoms):
     dropped = []
 
     last_residue_num = 0
-    last_chain = ''
 
     for i, atom in enumerate(atoms):
-
+        
         residue_name = atom[3]
         residue_chain = atom[4]
         residue_num = int (atom[5])
@@ -126,12 +142,17 @@ def getResidueDistance(atoms):
         if residue_name in residues_list:   
 
             if (residue_num != last_residue_num):
-
-                if (atom[2]==' C  '):
-                    cord_C = atom[6:9] 
-                    residues.append([residue_name + str (residue_num) + " " + residue_chain, cord_C]) 
-                    last_residue_num = residue_num   
-            
+                
+                if (atom[2].replace(" ","")=="CA"):#'C'or 'CA'
+                    if chain is None:
+                        cord_C = atom[6:9] 
+                        residues.append([residue_name + str (residue_num) + " " + residue_chain, cord_C]) 
+                        last_residue_num = residue_num   
+                    else: 
+                        if residue_chain == chain:
+                            cord_C = atom[6:9] 
+                            residues.append([residue_name + str (residue_num) + " " + residue_chain, cord_C]) 
+                            last_residue_num = residue_num  
         else:
           dropped.append(residue_name)
 
@@ -144,26 +165,37 @@ def associateResidueName(residues):
         dict_residue_name[str (i)] = residues[i, 0]
     return dict_residue_name
  
-def getResiduesSequence(pbdFilePath):
+def getResiduesSequence(pbdFilePath, chain = None):
   
     seq_res = []
     residues_list = ['ALA', 'CYS', 'ASP', 'GLU', 'PHE', 'GLY', 'HIS', 'ILE', 'LYS', 'LEU', 'MET', 'ASN', 'PRO', 'GLN', 'ARG', 'SER', 'THR', 'VAL', 'TRP', 'TYR']
-  
+    
     with open(pbdFilePath) as pdbfile:
         for line in pdbfile:
             if line[:6]=='SEQRES':
-                splitted_line = [line[19:22], line[23:26], line[27:30], line[31:34], line[35:38],
-                                line[39:42], line[43:46], line[47:50], line[51:54], line[55:58],
-                                line[59:62], line[63:66], line[67:70]]
-                for residue in splitted_line:
-                    if residue in residues_list:
-                        seq_res.append(residue)
+                if chain is None:
+                    splitted_line = [line[19:22], line[23:26], line[27:30], line[31:34], line[35:38],
+                                    line[39:42], line[43:46], line[47:50], line[51:54], line[55:58],
+                                    line[59:62], line[63:66], line[67:70]]
+                    for residue in splitted_line:
+                        if residue in residues_list:
+                            seq_res.append(residue)
+                else: 
+                    if chain == line[12]:
+                        if chain is None:
+                            splitted_line = [line[19:22], line[23:26], line[27:30], line[31:34], line[35:38],
+                                            line[39:42], line[43:46], line[47:50], line[51:54], line[55:58],
+                                            line[59:62], line[63:66], line[67:70]]
+                            for residue in splitted_line:
+                                if residue in residues_list:
+                                    seq_res.append(residue)
+                         
     return np.array(seq_res)
 
-def read_adj_mat(adj_filepath, p):
+def read_adj_mat(adj_filepath, p, min_, max_):
 
-    if (os.path.isfile("{}{}_adj_mat.txt".format(adj_filepath, p))):
-        adj = np.loadtxt("{}{}_adj_mat.txt".format(adj_filepath, p))
+    if (os.path.isfile("{}{}_adj_mat_{}_{}.txt".format(adj_filepath, p, min_, max_))):
+        adj = np.loadtxt("{}{}_adj_mat_{}_{}.txt".format(adj_filepath, p, min_, max_))
         return adj
     else: 
         raise Exception("Adj matrix for protein {} doesn't exists.".format(p))
@@ -197,12 +229,13 @@ def adjacent_matrix(output_path, residues, p, min_=4, max_=8):
         
     if not os.path.exists("{}Edgelists".format(output_path)):
         os.makedirs("{}Edgelists".format(output_path))
-    np.savetxt("{}Edgelists{}{}_edgelist.txt".format(output_path, add_slash_to_path, p), np.array(edge_list), fmt='%.2f')
+        
+    np.savetxt("{}Edgelists{}{}_edgelist_{}_{}.csv".format(output_path, add_slash_to_path, p, min_, max_), np.array(edge_list), fmt='%.2f')
     print("saved edge list")
         
     if not os.path.exists("{}Adj".format(output_path)):
         os.makedirs("{}Adj".format(output_path))
-    np.savetxt("{}Adj{}{}_adj_mat.txt".format(output_path, add_slash_to_path, p), adj, fmt='%.2f')
+    np.savetxt("{}Adj{}{}_adj_mat_{}_{}.txt".format(output_path, add_slash_to_path, p, min_, max_), adj, fmt='%.2f')
     print("saved adj matrix")
     
     return adj
@@ -327,11 +360,13 @@ def save_labels(output_path, labels, residue_names, p_name, method=None, d=None,
 
 #COMMUNITY EXTRACTION
 
-def extract_labels_from_coms(coms, algorithm_name):
+def extract_labels_from_coms(num_nodes,coms, algorithm_name):
   
-    n_coms = len(coms) #numero di comunità 
-    print("number of {} communities: {}".format(algorithm_name, n_coms))
-    labels = np.zeros((G.number_of_nodes(), 1))
+    if (algorithm_name != "Asyn FluidC"):
+        n_coms = len(coms) #numero di comunità 
+        print("number of {} communities: {}".format(algorithm_name, n_coms))
+    
+    labels = np.zeros((num_nodes, 1))
     dict_node_algorithm_com = dict ()
 
     for label, com in enumerate(coms):
@@ -344,12 +379,12 @@ def extract_labels_from_coms(coms, algorithm_name):
   
     return labels
 
-
 def louvain(G):
 
     louvain = algorithms.louvain(G)
     coms = louvain.communities
-    labels = extract_labels_from_coms(coms, "Louvain")
+    num_nodes = G.number_of_nodes()
+    labels = extract_labels_from_coms(num_nodes, coms, "Louvain")
 
     return labels
 
@@ -357,7 +392,8 @@ def leiden(G):
 
     leiden = algorithms.leiden(G)
     coms = leiden.communities
-    labels = extract_labels_from_coms(coms, "Leiden")
+    num_nodes = G.number_of_nodes()
+    labels = extract_labels_from_coms(num_nodes, coms, "Leiden")
 
     return labels  
  
@@ -365,35 +401,41 @@ def walktrap(G):
 
     walktrap = algorithms.walktrap(G)
     coms = walktrap.communities
-    labels = extract_labels_from_coms(coms, "Walktrap")
+    num_nodes = G.number_of_nodes()
+    labels = extract_labels_from_coms(num_nodes, coms, "Walktrap")
       
     return labels
   
 def greedy_modularity(G):
 
     coms = greedy_modularity_communities(G)
-    labels = extract_labels_from_coms(coms, "Greedy Modularity")
+    num_nodes = G.number_of_nodes()
+    labels = extract_labels_from_coms(num_nodes, coms, "Greedy Modularity")
 
     return labels
 
 def infomap(G):
     
     coms = algorithms.infomap(G).communities
-    labels = extract_labels_from_coms(coms, "Infomap")
+    num_nodes = G.number_of_nodes()
+    labels = extract_labels_from_coms(num_nodes, coms, "Infomap")
 
     return labels
 
 def asyn_fluidc(G, k):
   
     coms = asyn_fluidc_(G, k)
-    labels = extract_labels_from_coms(coms, "Asyn FluidC")
+    num_nodes = G.number_of_nodes()
+    print("number of Asyn FluidC communities: {}".format(k))
+    labels = extract_labels_from_coms(num_nodes, coms, "Asyn FluidC")
   
     return labels
   
 def spinglass(G):
 
     coms = algorithms.spinglass(G).communities
-    labels = extract_labels_from_coms(coms, "Spin Glass")
+    num_nodes = G.number_of_nodes()
+    labels = extract_labels_from_coms(num_nodes, coms, "Spin Glass")
   
     return labels
   
@@ -402,7 +444,7 @@ def spinglass(G):
 def degree_matrix(A):
   
     n = A.shape[0]
-    diag = np.zeros((n, n)) # basically dimensions of your graph
+    diag = np.zeros((n, n)) 
     rows, cols = A.nonzero()
     
     for row, col in zip(rows, cols):
@@ -698,9 +740,7 @@ def ssc_shimalik_laplacianeigenmaps(A, n_clusters = None, embedding="LaplacianEi
     labels = ssc_shimalik(A, n_clusters, embedding, d, beta)
     return labels
     
-    
 #NEW
-
 
 def betweenness(G, p, residue_names, n):
 

0.9.0-alpha/pcn_main.py
@@ -136,7 +136,10 @@ while (end==False):
                     
     else:
          raise Exception("'{}' is not a directory.".format(proteins_path))
-      
+    
+    min_ = float(input("Entering non covalent bonds threshold distance for PCN costruction: ") or 4.0)    
+    max_ = float(input("Entering only significant bonds threshold distance for PCN costruction : ") or 8.0)
+    
     if(initial_choice == 'adj'):
         
         if(use_config_choice==0):
@@ -148,64 +151,18 @@ while (end==False):
             adj_filespath = adj_filespath+add_slash_to_path
             
         if (is_dir_adj and is_dir_prot):
-            adj_list = [protein.casefold()+"_adj_mat.txt" for protein in proteins_list]
+            adj_list = [protein.casefold()+"_adj_mat_{}_{}.txt".format(min_, max_) for protein in proteins_list]
             pcn_final.checkIfFilesExists(adj_list, "adj", proteins_path, adj_filespath) #add_slash_to_path is path separator 
         else:
             raise Exception("'{}' is not a directory.".format(adj_filespath))
             
     elif (initial_choice == 'pdb'):
-        min_ = int(input("Entering non covalent bonds threshold distance for PCN costruction: ") or 4)    
-        max_ = int(input("Entering only significant bonds threshold distance for PCN costruction : ") or 8)
-        
+        pass
     else: 
         raise Exception("'initial_choice' input must be 'pdb' or 'adj' but '{}' given.".format(initial_choice))
-    ###NEW
+
     centrality_initial_choice =  int(input("Press 0 if you want to compute a node centrality measure: "))
-    if (centrality_initial_choice  == 0):
-        for protein in proteins_list:
-            
-            p_name = protein[:4]
-            print(("protein {}: CENTRALITY MEASURES COMPUTING NOW").format(p_name))
-                  
-            protein_path = proteins_path+p_name+".pdb"
-            atoms = pcn_final.readPDBFile(protein_path)
-            residues = pcn_final.getResidueDistance(atoms)
-            dict_residue_name = pcn_final.associateResidueName(residues)
-            residue_names = np.array(list (dict_residue_name.items()))
-                 
-            if(initial_choice == 'pdb'):
-                     
-                print("computing adjacency matrix for protein {}... (This may take time)".format(p_name))
-                A = pcn_final.adjacent_matrix(output_path, residues, p_name, min_, max_)
-      
-            else:     #'adj'           
-                print("reading adjacency matrix for protein {}...".format(p_name))
-                A = pcn_final.read_adj_mat(adj_filespath, p_name)
-            
-            G = nx.from_numpy_matrix(A)  
-            centralities_choice = str(input("Select one, a list (splitted by ','), or 'all' centrality measures from {}: ".format(str(supported_centralities_measures)))).casefold()
-                        
-            if centralities_choice == "all":
-                centralities_choice = supported_centralities_measures
-            elif(centralities_choice.split(',')):
-                centralities_choice =  [str(centrality_choice) for centrality_choice in centralities_choice.replace(" ","").split(",")]
-            else:
-                raise Exception("{} not supported".format(centralities_choice))
-                                                 
-            for centrality_choice in centralities_choice:
-            
-                print("Computing {} centrality measure on {} PCN".format(centrality_choice, p_name))
-                if(centrality_choice in supported_centralities_measures):
-                            
-                    method_to_call = getattr(pcn_final, centrality_choice)
-                                
-                    centrality_measures = method_to_call(G, p_name, residue_names, 10)
-                    pcn_pymol_scripts.pymol_plot_centralities(centrality_measures, protein_path, output_path, centrality_choice)
-                            
-                else:
-                    print("{} not supported".format(centrality_choice))
-        ###END NEW
-        
+    
     print('Please select the analysis approach: SPECTRAL|EMBEDDINGS|COMMUNITY'+'\n')
     print('Spectral will apply spectral clustering on PCN'+'\n')
     print('Embedding will apply embedding followed by clustering'+'\n')
@@ -290,22 +247,53 @@ while (end==False):
                 k_choice = str(input("Entering k for Asyn FluidC: Enter an int, a list of ints (split with ','): "))
                 if(k_choice.split(',')):
                     ks =  [int(item) for item in k_choice.replace(" ","").split(",")]
-        
-    if (len(algorithms_choice)>0):
-              
-        for protein in proteins_list:
             
-            p_name = protein[:4]   #p = 6vxx, protein = 6vxx.pdb, adj = 6vxx_adj_mat.txt
-            print(("protein {}: COMPUTING NOW").format(p_name))
-                  
-            protein_path = proteins_path+p_name+".pdb"
-            atoms = pcn_final.readPDBFile(protein_path)
-            residues = pcn_final.getResidueDistance(atoms)
-            dict_residue_name = pcn_final.associateResidueName(residues)
-            residue_names = np.array(list (dict_residue_name.items()))
-                 
+    for protein in proteins_list:
+        
+        p_name = protein[:4]
+        protein_path = proteins_path+p_name+".pdb"
+        atoms = pcn_final.readPDBFile(protein_path)
+        residues = pcn_final.getResidueDistance(atoms)
+        dict_residue_name = pcn_final.associateResidueName(residues)
+        residue_names = np.array(list (dict_residue_name.items()))
+        
+        if(initial_choice == 'pdb'):
+                     
+            print("computing adjacency matrix for protein {}... (This may take time)".format(p_name))
+            A = pcn_final.adjacent_matrix(output_path, residues, p_name, min_, max_)
+      
+        else:     #'adj'           
             print("reading adjacency matrix for protein {}...".format(p_name))
-            A = pcn_final.read_adj_mat(adj_filespath, p_name)
+            A = pcn_final.read_adj_mat(adj_filespath, p_name, min_, max_)
+            
+        if (centrality_initial_choice  == 0):
+            
+            print(("protein {}: CENTRALITY MEASURES COMPUTING NOW").format(p_name))
+                              
+            G = nx.from_numpy_matrix(A)  
+            centralities_choice = str(input("Select one, a list (splitted by ','), or 'all' centrality measures from {}: ".format(str(supported_centralities_measures)))).casefold()
+                        
+            if centralities_choice == "all":
+                centralities_choice = supported_centralities_measures
+            elif(centralities_choice.split(',')):
+                centralities_choice =  [str(centrality_choice) for centrality_choice in centralities_choice.replace(" ","").split(",")]
+            else:
+                raise Exception("{} not supported".format(centralities_choice))
+                                                 
+            for centrality_choice in centralities_choice:
+            
+                print("Computing {} centrality measure on {} PCN".format(centrality_choice, p_name))
+                if(centrality_choice in supported_centralities_measures):
+                            
+                    method_to_call = getattr(pcn_final, centrality_choice)
+                                
+                    centrality_measures = method_to_call(G, p_name, residue_names, 10)
+                    pcn_pymol_scripts.pymol_plot_centralities(centrality_measures, protein_path, output_path, centrality_choice)
+                            
+                else:
+                    print("{} not supported".format(centrality_choice))
+        
+        if (len(algorithms_choice)>0):
                       
             if ((type_choice == 'spectral') or (type_choice == 'embeddings')):              
                 